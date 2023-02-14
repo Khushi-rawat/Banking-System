9bd4211fd556271af7821779e100d42777211470
Khushi-rawat <83412594+Khushi-rawat@users.noreply.github.com>
2023-02-14 21:40:56 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Banking System</title>
+    <style>
+        *{
+            padding: 0;
+            margin: 0;
+        }
+        body{
+            background-image: linear-gradient(to right, rgba(109, 179, 242, .6), rgba(242, 242, 242, .6)), url('img/background.jpg');
+background-size: cover;
+background-repeat: repeat, no-repeat;
+            color: darkblue;
+            font-family: arial;
+        }
+        .main{
+            margin:10%;
+            font-weight: bolder;
+            font-size: larger;
+        }
+        button{
+            margin-top: 2%;
+            padding: 2%;
+            border-radius: 30px;
+            background-color: yellow;
+            color: blue;
+            font-weight: bold;
+            font-size: x-large;
+            cursor: pointer;
+        }
+        button:hover{
+            background-color: #FFD700;
+        }
+        </style>
+</head>
+<body>
+    <div class="main">
+    <h1>
+        Welcome to The Sparks Foundation <br>Basic Banking System
+</h1>
+<a href="./customer.php"><button>View All Customers</button></a>
+    </div>
+    </body>
+</html>
